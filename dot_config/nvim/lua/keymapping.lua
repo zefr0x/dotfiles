98e6cc1ebfe85e23ece50fb8d2e098265033af7b
zer0-x <65136727+ZER0-X@users.noreply.github.com>
2022-08-14 23:14:15 +0300

    Migrate to vim.keymap.set to map native lua function

--- dot_config/nvim/lua/keymapping.lua
+++ dot_config/nvim/lua/keymapping.lua
@@ -1,8 +1,11 @@
+-- To avoid (`vim` is not defined) erorr in every line when using LSP.
+local vim = vim
+
 --- Set keymap
 -- @param[opts={silent = true, noremap = true}]
 local function map(mode, keys, action, opts)
-    vim.api.nvim_set_keymap (mode, keys, action,
-        opts or {silent = true, noremap = true}
+    vim.keymap.set(mode, keys, action,
+    opts or {silent = true, noremap = true}
     )
 end
 
@@ -19,21 +22,21 @@ map("n", "tt", ":tabnew<CR>")
 ------------ Plugins KeyMapping -------------
 
 -- lspconfig
-map("n", "<leader>D", "<cmd>lua vim.lsp.buf.declaration()<CR>")
-map("n", "<leader>d", "<cmd>lua vim.lsp.buf.definition()<CR>")
-map("n", "<leader>m", "<cmd>lua vim.lsp.buf.hover()<CR>")
-map("n", "<leader>r", "<cmd>lua vim.lsp.buf.references()<CR>")
-map("n", "<leader>s", "<cmd>lua vim.lsp.buf.signature_help()<CR>")
-map("n", "<leader>i", "<cmd>lua vim.lsp.buf.implementation()<CR>")
-map("n", "<leader>t", "<cmd>lua vim.lsp.buf.type_definition()<CR>")
-map("n", "<leader>gw", "<cmd>lua vim.lsp.buf.document_symbol()<CR>")
-map("n", "<leader>gW", "<cmd>lua vim.lsp.buf.workspace_symbol()<CR>")
-map("n", "<leader>a", "<cmd>lua vim.lsp.buf.code_action()<CR>")
-map("n", "<leader>ee", "<cmd>lua vim.lsp.util.show_line_diagnostics()<CR>")
-map("n", "<leader>n", "<cmd>lua vim.lsp.buf.rename()<CR>")
-map("n", "<leader>=", "<cmd>lua vim.lsp.buf.formatting()<CR>")
-map("n", "<leader>ai", "<cmd>lua vim.lsp.buf.incoming_calls()<CR>")
-map("n", "<leader>ao", "<cmd>lua vim.lsp.buf.outgoing_calls()<CR>")
+map("n", "<leader>D", vim.lsp.buf.declaration)
+map("n", "<leader>d", vim.lsp.buf.definition)
+map("n", "<leader>m", vim.lsp.buf.hover)
+map("n", "<leader>r", vim.lsp.buf.references)
+map("n", "<leader>s", vim.lsp.buf.signature_help)
+map("n", "<leader>i", vim.lsp.buf.implementation)
+map("n", "<leader>t", vim.lsp.buf.type_definition)
+map("n", "<leader>gw", vim.lsp.buf.document_symbol)
+map("n", "<leader>gW", vim.lsp.buf.workspace_symbol)
+map("n", "<leader>a", vim.lsp.buf.code_action)
+map("n", "<leader>ee", vim.lsp.diagnostic.show_line_diagnostics)
+map("n", "<leader>n", vim.lsp.buf.rename)
+map("n", "<leader>=", vim.lsp.buf.formatting)
+map("n", "<leader>ai", vim.lsp.buf.incoming_calls)
+map("n", "<leader>ao", vim.lsp.buf.outgoing_calls)
 
 
 -- bufferline.nvim
@@ -41,16 +44,16 @@ map("n", "<leader>bp", "<cmd>BufferLinePick<cr>")
 map("n", "<leader>bc", "<cmd>BufferLinePickClose<cr>")
 
 -- nvim-tree.lua
-map("n", "<F3>", "<cmd>:NvimTreeToggle<cr>")
+map("n", "<F3>", "<cmd>NvimTreeToggle<cr>")
 
 
 -- trouble
-map("n", "<F5>", "<cmd>:TroubleToggle document_diagnostics<cr>")
+map("n", "<F5>", "<cmd>TroubleToggle document_diagnostics<cr>")
 -- TODO: Conseder deleting key map
-map("n", "<F6>", "<cmd>:TroubleToggle loclist<cr>")
+map("n", "<F6>", "<cmd>TroubleToggle loclist<cr>")
 
 -- todo-comments.nvim
-map("n", "<F7>", "<cmd>:TodoTrouble<cr>")
+map("n", "<F7>", "<cmd>TodoTrouble<cr>")
 
 
 
@@ -63,7 +66,7 @@ map("n", "<c-n>", "<Plug>(YankyCycleForward)")
 map("n", "<c-n>", "<plug>(yankycyclebackward)")
 
 -- nvim-surround
-map("n", "<F4>", "<cmd>:SymbolsOutline<cr>")
+map("n", "<F4>", "<cmd>SymbolsOutline<cr>")
 
 -- telescope.nvim
 map("n", "<leader>ff", "<cmd>Telescope find_files<cr>")
@@ -76,11 +79,39 @@ map("n", "<c-h>", "<cmd>Telescope yank_history<cr>")
 
 
 -- hop.nvim
-map("n", "<leader>f", "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR })<cr>")
-map("n", "<leader>F", "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR })<cr>")
-map("n", "<leader>t", "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, hint_offset = -1 })<cr>")
-map("n", "<leader>T", "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, hint_offset = 1 })<cr>")
-map("n", "<leader>k", "<cmd>lua require'hop'.hint_lines_skip_whitespace({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR })<cr>")
-map("n", "<leader>j", "<cmd>lua require'hop'.hint_lines_skip_whitespace({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR })<cr>")
-map("n", "<leader>w", "<cmd>lua require'hop'.hint_words({ current_line_only = true })<cr>")
+map("n", "<leader>f",
+    function()
+        require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR })
+    end
+)
+map("n", "<leader>F",
+    function()
+        require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR })
+    end
+)
+map("n", "<leader>t",
+    function()
+        require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, hint_offset = -1 })
+    end
+)
+map("n", "<leader>T",
+    function()
+        require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, hint_offset = 1 })
+    end
+)
+map("n", "<leader>k",
+    function()
+        require'hop'.hint_lines_skip_whitespace({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR })
+    end
+)
+map("n", "<leader>j",
+    function()
+        require'hop'.hint_lines_skip_whitespace({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR })
+    end
+)
+map("n", "<leader>w",
+    function()
+        require'hop'.hint_words({ current_line_only = true })
+    end
+)
 
