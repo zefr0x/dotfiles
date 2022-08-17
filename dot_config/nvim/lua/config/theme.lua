6a62c5eb84c9d8b049e645edad6a1bcd7ac4a5ee
zer0-x <65136727+ZER0-X@users.noreply.github.com>
2022-08-18 02:26:33 +0300

    nvim: Change FidgetTask highlight color

--- dot_config/nvim/lua/config/theme.lua
+++ dot_config/nvim/lua/config/theme.lua
@@ -5,5 +5,5 @@ require('github-theme').setup {
 }
 
 -- Fix fidget highliting and use better color
-vim.api.nvim_set_hl(0, "FidgetTask", {fg="#75beff"})
+vim.api.nvim_set_hl(0, "FidgetTask", {fg="#58a6ff"})
 
