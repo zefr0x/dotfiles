0cb9c3283015adb3d8d67e89196ea1998457c8ae
zer0-x <zer0-x.7ty50@aleeas.com>
2022-12-30 16:29:27 +0300

    Fix lua language server show message every time lua file is opened
    More details: https://github.com/neovim/nvim-lspconfig/issues/1700

--- dot_config/nvim/lua/plugin/lsp.lua
+++ dot_config/nvim/lua/plugin/lsp.lua
@@ -56,6 +56,7 @@ lspconfig.sumneko_lua.setup({
 			},
 			workspace = {
 				library = vim.api.nvim_get_runtime_file("", true),
+				checkThirdParty = false,
 			},
 			telemetry = {
 				enable = false,
