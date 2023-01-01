c7a10e6d9772002696868371b0d5c94afd28b8c8
zer0-x <zer0-x.7ty50@aleeas.com>
2023-01-01 21:03:20 +0300

    nvim: Start using emmet-ls LSP

--- dot_config/nvim/lua/plugin/lsp.lua
+++ dot_config/nvim/lua/plugin/lsp.lua
@@ -91,6 +91,21 @@ lspconfig.html.setup({
 	cmd = { "vscode-html-languageserver", "--stdio" },
 })
 
+-- emmet-ls
+lspconfig.emmet_ls.setup({
+    -- on_attach = custom_on_attach,
+    capabilities = capabilities,
+    filetypes = { 'html', "htmldjango", 'typescriptreact', 'javascriptreact', 'css', 'sass', 'scss', 'less' },
+    init_options = {
+      html = {
+        options = {
+          -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
+          ["bem.enabled"] = true,
+        },
+      },
+    }
+})
+
 -- Set icons for diagnostics in the gutter.
 local signs = {
 	Error = " ",
