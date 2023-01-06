f105f00d0fad31fe8904833dd583e86941771795
zer0-x <zer0-x.7ty50@aleeas.com>
2023-01-06 15:26:09 +0300

    nvim: Configure nvim-tree

--- dot_config/nvim/lua/plugin/file_browser.lua
+++ dot_config/nvim/lua/plugin/file_browser.lua
@@ -1,2 +1,10 @@
 -- nvim-tree.lua
-require("nvim-tree").setup({})
+require("nvim-tree").setup({
+	-- sort_by = "name",
+	renderer = {
+		special_files = { "Cargo.toml", "README.md", "readme.md" },
+	},
+	filters = {
+		custom = { "^.git$", "^.env$" },
+	},
+})
