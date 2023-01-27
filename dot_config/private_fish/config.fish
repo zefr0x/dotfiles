f1a431e82625cd26688fe9e8541e4185d24edb2c
zer0-x <zer0-x.7ty50@aleeas.com>
2023-01-27 17:21:09 +0300

    fish: Fix the theme section in the config

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -38,47 +38,47 @@ if status is-interactive
 
 # ------------ Fish Theme --------------
 # Syntax Highlighting
-set fish_color_normal normal
-set fish_color_command FF9400
-set fish_color_keyword FF9400
-set fish_color_quote BF9C30
-set fish_color_redirection BF5B30
-set fish_color_end FF4C00
-set fish_color_error FFDD73
-set fish_color_param FFC000
-set fish_color_valid_path --underline
-set fish_color_option FFC000
-set fish_color_comment A63100
-set fish_color_selection white --bold --background=brblack
-set fish_color_operator 00a6b2
-set fish_color_escape 00a6b2
-set fish_color_autosuggestion FFC473
-set fish_color_cwd green
-set fish_color_cwd_root red
-set fish_color_user brgreen
-set fish_color_host normal
-set fish_color_host_remote yellow
-set fish_color_status red
-set fish_color_cancel --reverse
-set fish_color_search_match bryellow --background=brblack
+    set fish_color_normal normal
+    set fish_color_command FF9400
+    set fish_color_keyword FF9400
+    set fish_color_quote BF9C30
+    set fish_color_redirection BF5B30
+    set fish_color_end FF4C00
+    set fish_color_error FFDD73
+    set fish_color_param FFC000
+    set fish_color_valid_path --underline
+    set fish_color_option FFC000
+    set fish_color_comment A63100
+    set fish_color_selection white --bold --background=brblack
+    set fish_color_operator 00a6b2
+    set fish_color_escape 00a6b2
+    set fish_color_autosuggestion FFC473
+    set fish_color_cwd green
+    set fish_color_cwd_root red
+    set fish_color_user brgreen
+    set fish_color_host normal
+    set fish_color_host_remote yellow
+    set fish_color_status red
+    set fish_color_cancel --reverse
+    set fish_color_search_match bryellow --background=brblack
 
 # Pager Color
-set fish_pager_color_progress brwhite --background=cyan
-set fish_pager_color_background
-set fish_pager_color_prefix normal --bold --underline
-set fish_pager_color_completion normal
-set fish_pager_color_description B3A06D yellow
-set fish_pager_color_selected_background --background=brblack
-set fish_pager_color_selected_prefix
-set fish_pager_color_selected_completion
-set fish_pager_color_selected_description
-set fish_pager_color_secondary_background
-set fish_pager_color_secondary_prefix
-set fish_pager_color_secondary_completion
-set fish_pager_color_secondary_description
+    set fish_pager_color_progress brwhite --background=cyan
+    set fish_pager_color_background
+    set fish_pager_color_prefix normal --bold --underline
+    set fish_pager_color_completion normal
+    set fish_pager_color_description B3A06D yellow
+    set fish_pager_color_selected_background --background=brblack
+    set fish_pager_color_selected_prefix
+    set fish_pager_color_selected_completion
+    set fish_pager_color_selected_description
+    set fish_pager_color_secondary_background
+    set fish_pager_color_secondary_prefix
+    set fish_pager_color_secondary_completion
+    set fish_pager_color_secondary_description
 
 # `dirh`
-set fish_color_history_current --bold
+    set fish_color_history_current --bold
 
 # ----------- Abbreviations ------------
     abbr -a s "sudo"
