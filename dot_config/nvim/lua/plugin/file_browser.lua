3801738fddb3e5949cfc71af5a6a141170602a37
zer0-x <zer0-x.7ty50@aleeas.com>
2023-02-28 22:48:22 +0300

    nvim: Fix and improve natural sorting in file browser

--- dot_config/nvim/lua/plugin/file_browser.lua
+++ dot_config/nvim/lua/plugin/file_browser.lua
@@ -7,20 +7,25 @@ local function natural_cmp(left, right)
 	left = left.name:lower()
 	right = right.name:lower()
 
-	for i = 1, string.len(left), 1 do
+	if left == right then
+		return false
+	end
+
+	for i = 1, math.max(string.len(left), string.len(right)), 1 do
 		local l = string.sub(left, i, -1)
 		local r = string.sub(right, i, -1)
 
-		if l == r then
-			return false
-		elseif type(tonumber(string.sub(l, 1, 1))) == "number" and type(tonumber(string.sub(r, 1, 1))) == "number" then
-			return tonumber(string.match(l, "^[0-9]+")) < tonumber(string.match(r, "^[0-9]+"))
-		elseif l[1] == r[1] then
-			goto continue
-		else
+		if type(tonumber(string.sub(l, 1, 1))) == "number" and type(tonumber(string.sub(r, 1, 1))) == "number" then
+			local l_number = tonumber(string.match(l, "^[0-9]+"))
+			local r_number = tonumber(string.match(r, "^[0-9]+"))
+
+			-- When the same number in the start of the window, there might be numbers later.
+			if l_number ~= r_number then
+				return l_number < r_number
+			end
+		elseif string.sub(l, 1, 1) ~= string.sub(r, 1, 1) then
 			return l < r
 		end
-		::continue::
 	end
 end
 
