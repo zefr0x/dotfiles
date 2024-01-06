2574d9e174710b63923977671b61fb0ef5fdb789
zefr0x <zer0-x.7ty50@aleeas.com>
2024-01-06 23:53:58 +0300

    nvim: replace packer.nvim with lazy.nvim

--- tools/nvim/init.lua
+++ tools/nvim/init.lua
@@ -3,40 +3,17 @@ local g = vim.g
 ------------ Disable Providers --------------
 g.loaded_python3_provider = 0
 
---------- Disable builtin plugins -----------
-g.loaded_zip = 1
-g.loaded_zipPlugin = 1
-g.loaded_tar = 1
-g.loaded_tarPlugin = 1
--- g.loaded_gzip = 1
-
-g.loaded_vimball = 1
-g.loaded_vimballPlugin = 1
-
--- Currently broken, https://github.com/nvim-treesitter/nvim-treesitter/issues/1049
-g.loaded_2html_plugin = 1
-
-g.loaded_remote_plugins = 1
-vim["loaded_spellfile_plugin"] = 1
-vim.g.loaded_tutor_mode_plugin = 1
-
-g.loaded_netrw = 1
-g.loaded_netrwPlugin = 1
-g.loaded_netrwSettings = 1
-g.loaded_netrwFileHandlers = 1
-g.loaded_netrw_gitignore = 1
-
 ---------------- Base configs ----------------
 require("base_config")
-
----------------- Load Plugins ----------------
-require("plugin_loading")
 
 ----------------- Load Theme -----------------
 vim.cmd([[colorscheme lunaperche]])
 
 -- Some extra changes on top of the theme.
 require("theme")
+
+---------------- Load Plugins ----------------
+require("plugin_loading")
 
 ---------------- Set KeyMaps -----------------
 require("keymapping")
--- tools/nvim/lua/plugin_loading.lua
+++ tools/nvim/lua/plugin_loading.lua
@@ -1,24 +1,20 @@
--- Bootstrapping packer.nvim
-local ensure_packer = function()
-	local install_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-	if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-		vim.fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
-		vim.cmd([[packadd packer.nvim]])
-		return true
-	end
-	return false
+-- Bootstrapping lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
+vim.opt.rtp:prepend(lazypath)
 
-local packer_bootstrap = ensure_packer()
-
-return require("packer").startup(function(use)
-	use("wbthomason/packer.nvim")
-
-	-- Profiling Vim and Neovim startup time
-	-- use("dstein64/vim-startuptime")
-
+require("lazy").setup({
 	-- Collection of configurations for built-in LSP client
-	use({
+	{
 		"neovim/nvim-lspconfig",
 		ft = {
 			"rust",
@@ -35,153 +31,129 @@ return require("packer").startup(function(use)
 			"html",
 			"json",
 		},
+		dependencies = {
+			-- LSP inlay hints.
+			{
+				"lvimuser/lsp-inlayhints.nvim",
+				-- FIX: Load it only in supported file types.
+				event = "LspAttach",
+				ft = {
+					"rust",
+					"c",
+					"cpp",
+					"java",
+				},
+				config = function()
+					require("plugin.inlyhints")
+				end,
+			},
+		},
 		config = function()
 			require("plugin.lsp")
 		end,
-	})
-	-- LSP inlay hints.
-	use({
-		"lvimuser/lsp-inlayhints.nvim",
-		event = "LspAttach",
-		config = function()
-			require("plugin.inlyhints")
-		end,
-	})
-	-- LSP source for nvim-cmp
-	use({
-		"hrsh7th/cmp-nvim-lsp",
-		module = "cmp_nvim_lsp",
-	})
-	-- Simple winbar/statusline plugin that shows your current code context
-	use({
-		"SmiteshP/nvim-navic",
-		module = "nvim-navic",
-	})
+	},
 
 	-- An asynchronous linter plugin for Neovim complementary to the built-in Language Server Protocol support
-	use({
+	{
 		"mfussenegger/nvim-lint",
 		ft = { "c", "cpp", "python", "tex", "lua", "javascript", "typescript", "html", "css" },
 		config = function()
 			require("plugin.lint")
 		end,
-	})
+	},
 
 	-- Autocompletion
-	use({
+	{
 		"hrsh7th/nvim-cmp",
 		event = "InsertEnter",
+		dependencies = {
+			-- LSP source for nvim-cmp
+			"hrsh7th/cmp-nvim-lsp",
+			-- cmp-nvim-lsp-signature-help
+			"hrsh7th/cmp-nvim-lsp-signature-help",
+			-- nvim-cmp source for textDocument/documentSymbol via nvim-lsp
+			"hrsh7th/cmp-nvim-lsp-document-symbol",
+			-- nvim-cmp source for path
+			"hrsh7th/cmp-path",
+			-- nvim-cmp source for vim's cmdline
+			"hrsh7th/cmp-cmdline",
+			-- nvim-cmp source for buffer words
+			"hrsh7th/cmp-buffer",
+			-- Add latex symbol support for nvim-cmp
+			-- "kdheepak/cmp-latex-symbols",
+			-- Snippets source for nvim-cmp
+			{
+				"saadparwaiz1/cmp_luasnip",
+				dependencies = {
+					-- Snippets plugin
+					{
+						"L3MON4D3/LuaSnip",
+						dependencies = { "rafamadriz/friendly-snippets" },
+					},
+				},
+			},
+		},
 		config = function()
 			require("plugin.completion")
 		end,
-	})
-	-- Autopairs for () and {} etc...
-	use({
-		"windwp/nvim-autopairs",
-		module = "nvim-autopairs",
-		config = function()
-			require("plugin.auto_pair")
-		end,
-	})
-	-- Snippets source for nvim-cmp
-	use({
-		"saadparwaiz1/cmp_luasnip",
-		after = "nvim-cmp",
-	})
-	use({
-		-- Snippets plugin
-		"L3MON4D3/LuaSnip",
-		module = "luasnip",
-	})
-	use({
-		"rafamadriz/friendly-snippets",
-		after = "LuaSnip",
-	})
-	-- cmp-nvim-lsp-signature-help
-	use({
-		"hrsh7th/cmp-nvim-lsp-signature-help",
-		after = "nvim-cmp",
-	})
-	-- nvim-cmp source for textDocument/documentSymbol via nvim-lsp
-	use({
-		"hrsh7th/cmp-nvim-lsp-document-symbol",
-		after = "nvim-cmp",
-	})
-	-- nvim-cmp source for path
-	use({
-		"hrsh7th/cmp-path",
-		after = "nvim-cmp",
-	})
-	-- nvim-cmp source for vim's cmdline
-	use({
-		"hrsh7th/cmp-cmdline",
-		after = "nvim-cmp",
-	})
-	-- nvim-cmp source for buffer words
-	use({
-		"hrsh7th/cmp-buffer",
-		after = "nvim-cmp",
-	})
-	-- Add latex symbol support for nvim-cmp
-	-- "kdheepak/cmp-latex-symbols",
+	},
 
 	--  Help managing dependencies in Cargo.toml files
-	use({
+	{
 		"saecki/crates.nvim",
 		event = "BufReadPost Cargo.toml",
-		tag = "v0.3.0",
-		requires = "nvim-lua/plenary.nvim",
+		version = "v0.3.0",
+		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
 			require("plugin.crates")
 		end,
-	})
+	},
 
 	-- TODO: Configure debug adapter protocol client for neovim
-	-- use "mfussenegger/nvim-dap"
+	-- "mfussenegger/nvim-dap",
 
 	-- Pretty diagnostics, references, telescope results, quickfix and location list to help you solve all the trouble your code is causing
-	use({
+	{
 		"folke/trouble.nvim",
-		event = "CmdUndefined TroubleToggle",
-		requires = { "nvim-tree/nvim-web-devicons" },
+		cmd = "TroubleToggle",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
 		config = function()
 			require("plugin.trouble_diagnostics")
 		end,
-	})
+	},
 	-- Find, Filter, Preview, Pick. All lua, all the time
-	-- plenary: full; complete; entire; absolute; unqualified. All the lua functions I don't want to write twice.
-	-- telescope-fzf-native.nvim: FZF sorter for telescope written in c
-	-- https://github.com/BurntSushi/ripgrep is required also.
-	use({
+	-- https://github.com/BurntSushi/ripgrep is required.
+	{
 		"nvim-telescope/telescope.nvim",
-		event = "CmdUndefined Telescope",
+		cmd = "Telescope",
 		branch = "0.1.x",
-		requires = {
+		dependencies = {
+			-- plenary: full; complete; entire; absolute; unqualified. All the lua functions I don't want to write twice.
 			"nvim-lua/plenary.nvim",
+			-- telescope-fzf-native.nvim: FZF sorter for telescope written in c
 			{
 				"nvim-telescope/telescope-fzf-native.nvim",
-				module = "telescope._extensions.fzf",
-				run = "make",
+				build = "make",
 			},
 			"nvim-tree/nvim-web-devicons",
 		},
 		config = function()
 			require("plugin.telescope")
 		end,
-	})
+	},
 	-- Comments todo list
-	-- https://github.com/BurntSushi/ripgrep is required also.
-	use({
+	-- https://github.com/BurntSushi/ripgrep is required.
+	{
 		"folke/todo-comments.nvim",
 		event = "BufEnter",
-		requires = { "nvim-lua/plenary.nvim" },
+		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
 			require("plugin.todo_comments")
 		end,
-	})
+	},
 
 	-- Nvim Treesitter configurations and abstraction layer
-	use({
+	{
 		"nvim-treesitter/nvim-treesitter",
 		ft = {
 			"rust",
@@ -214,186 +186,209 @@ return require("packer").startup(function(use)
 			"java",
 			"kotlin",
 		},
-		module = "nvim-treesitter",
-		run = function()
+		build = function()
 			require("nvim-treesitter.install").update({ with_sync = true })
 		end,
+		dependencies = {
+			-- Determines the text object you meant based on your location in the syntax tree
+			"nvim-treesitter/nvim-treesitter-textobjects",
+			-- Rainbow parentheses for neovim using tree-sitter
+			"https://git.sr.ht/~p00f/nvim-ts-rainbow",
+			-- Highlight arguments' definitions and usages, using Treesitter
+			{
+				"m-demare/hlargs.nvim",
+				config = function()
+					require("plugin.hlargs")
+				end,
+			},
+			-- Autopairs for () and {} etc...
+			{
+				"windwp/nvim-autopairs",
+				config = function()
+					require("plugin.auto_pair")
+				end,
+			},
+			-- Use treesitter to auto close and auto rename html tag
+			{
+				"windwp/nvim-ts-autotag",
+				-- HTML files may have diffrent file types.
+				event = "InsertEnter *.html",
+				config = function()
+					require("plugin.ts_autotag")
+				end,
+			},
+			-- indent guides for neovim
+			{
+				"lukas-reineke/indent-blankline.nvim",
+				config = function()
+					require("plugin.indent_blankline")
+				end,
+			},
+		},
 		config = function()
 			require("plugin.treesitter")
 		end,
-	})
-	-- Rainbow parentheses for neovim using tree-sitter
-	use({
-		"https://git.sr.ht/~p00f/nvim-ts-rainbow",
-		after = "nvim-treesitter",
-	})
-	-- Highlight arguments' definitions and usages, using Treesitter
-	use({
-		"m-demare/hlargs.nvim",
-		after = "nvim-treesitter",
-		config = function()
-			require("plugin.hlargs")
-		end,
-	})
-	-- Determines the text object you meant based on your location in the syntax tree
-	use({
-		"nvim-treesitter/nvim-treesitter-textobjects",
-		after = "nvim-treesitter",
-	})
-	-- Use treesitter to auto close and auto rename html tag
-	use({
-		"windwp/nvim-ts-autotag",
-		event = "InsertEnter *.html",
-		after = "nvim-treesitter",
-		config = function()
-			require("plugin.ts_autotag")
-		end,
-	})
+	},
 	-- Paint colors text in for #000000 or rgb(0,0,0) or ... with the real colors
-	use({
+	{
 		"NvChad/nvim-colorizer.lua",
 		ft = { "css", "scss", "lua" },
 		config = function()
 			require("plugin.colorizer")
 		end,
-	})
+	},
 	-- vim-polyglot Highlight matching html tags
 	-- TODO: Find plugin like this one
-	-- use "valloric/MatchTagAlways"
-
-	-- indent guides for neovim
-	use({
-		"lukas-reineke/indent-blankline.nvim",
-		after = "nvim-treesitter",
-		config = function()
-			require("plugin.indent_blankline")
-		end,
-	})
+	-- {
+	-- 	"valloric/MatchTagAlways",
+	-- 	event = "InsertEnter *.html",
+	-- },
 
 	-- Speed and easy motions.
-	use({
+	{
 		"phaazon/hop.nvim",
-		module = "hop",
+		-- PERF: Load based on key bindings.
+		-- keys = {}
+		event = "BufEnter",
 		branch = "v2",
 		config = function()
 			require("plugin.hop_motions")
 		end,
-	})
+	},
 	-- Surround
-	use({
+	{
 		"kylechui/nvim-surround",
 		event = "BufEnter",
 		config = function()
 			require("plugin.surround")
 		end,
-	})
+	},
 	-- Comment code easily
-	use({
+	{
 		"numToStr/Comment.nvim",
 		event = "BufEnter",
 		config = function()
 			require("plugin.comment")
 		end,
-	})
+	},
 
 	-- Better file browser
-	use({
+	{
 		"kyazdani42/nvim-tree.lua",
-		event = "CmdUndefined NvimTreeToggle",
-		requires = { "nvim-tree/nvim-web-devicons" },
+		cmd = "NvimTreeToggle",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
 		config = function()
 			require("plugin.file_browser")
 		end,
-	})
+	},
 	-- Class/module browser
-	use({
+	{
 		"simrat39/symbols-outline.nvim",
-		event = "CmdUndefined SymbolsOutline",
-		requires = { "nvim-treesitter/nvim-treesitter" },
+		cmd = "SymbolsOutline",
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
 		config = function()
 			require("plugin.symbols_outline")
 		end,
-	})
+	},
 
 	-- Git integration for buffers
-	use({
+	{
 		"lewis6991/gitsigns.nvim",
+		-- PERF: Load only when inside git repo.
 		event = "BufEnter",
 		config = function()
 			require("plugin.gitsigns")
 		end,
-	})
+	},
 	-- TODO: Configure testing framework
-	-- use "nvim-neotest/neotest"
+	-- "nvim-neotest/neotest",
 
 	-- Lualine
-	use({
+	{
 		"nvim-lualine/lualine.nvim",
 		event = "UIEnter",
-		requires = {
+		dependencies = {
 			"nvim-tree/nvim-web-devicons",
 			-- LSP Progress lualine componenet
 			"arkav/lualine-lsp-progress",
+			-- Simple winbar/statusline plugin that shows your current code context
+			{
+				"SmiteshP/nvim-navic",
+			},
 		},
 		config = function()
 			require("plugin.lualine")
 		end,
-	})
+	},
 	--  A snazzy bufferline/tabline
-	use({
+	{
 		"akinsho/bufferline.nvim",
 		event = "BufEnter",
-		tag = "v3.*",
-		requires = "nvim-tree/nvim-web-devicons",
+		version = "v3.*",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
 		config = function()
 			require("plugin.bufferline")
 		end,
-	})
+	},
 
 	-- Improved Yank and Put functionalities for Neovim
-	use({
+	{
 		"gbprod/yanky.nvim",
 		event = "BufEnter",
 		config = function()
 			require("plugin.yanky")
 		end,
-	})
+	},
 	-- The undo history visualizer for Vim
 	-- TODO: Find a lua alternative.
-	use({
+	{
 		"mbbill/undotree",
 		event = "BufEnter",
 		config = function()
 			require("plugin.undotree")
 		end,
-	})
+	},
 	-- Auto save fiels to disk
-	use({
+	{
 		"Pocco81/auto-save.nvim",
 		event = { "BufReadPost", "BufWritePost" },
 		config = function()
 			require("plugin.auto_save")
 		end,
-	})
+	},
 	-- Intelligently reopen files at your last edit position in Vim
-	use({
+	{
 		"ethanholz/nvim-lastplace",
 		event = "BufReadPre",
 		config = function()
 			require("plugin.lastplace")
 		end,
-	})
+	},
 	-- Smooth scrolling neovim plugin written in lua
-	use({
+	{
 		"karb94/neoscroll.nvim",
 		event = { "CursorMoved", "CursorMovedI" },
 		config = function()
 			require("plugin.neoscroll")
 		end,
-	})
-
-	-- Automatically set up configuration after cloning packer.nvim
-	if packer_bootstrap then
-		require("packer").sync()
-	end
-end)
+	},
+}, {
+	lockfile = vim.fn.expand("~/.dotfiles/tools/nvim/lazy-lock.json"),
+	performance = {
+		rtp = {
+			disabled_plugins = {
+				"gzip",
+				-- "matchit",
+				-- "matchparen",
+				"netrwPlugin",
+				"tarPlugin",
+				"tohtml",
+				"tutor",
+				"zipPlugin",
+				"spellfile",
+				"health",
+				"rplugin",
+			},
+		},
+	},
+})
