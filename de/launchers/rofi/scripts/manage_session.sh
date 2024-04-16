11496b5b5b2013c9eb16959e30dcff4fa8e34b4c
zefr0x <zer0-x.7ty50@aleeas.com>
2024-04-16 04:35:40 +0300

    rofi: general style improvement and some logic

--- de/launchers/rofi/scripts/manage_session.sh
+++ de/launchers/rofi/scripts/manage_session.sh
@@ -2,12 +2,12 @@
 # A rofi shell script to manage the session.
 
 if [ "$ROFI_RETV" = "0" ]; then
-	echo " Lock\0info\0037lock\n"
-	echo "󰗽 Logout\0info\0037logout\n"
-	echo "󰒲 Suspend\0info\0037suspend\n"
-	echo "󰋊 Hibernate\0info\0037hibernate\n"
-	echo "󰜉 Reboot\0info\0037reboot\n"
-	echo " Shutdown\0info\0037shutdown\n"
+	echo "Lock\0icon\0037system-lock-screen-symbolic\0037info\0037lock\n"
+	echo "Logout\0icon\0037system-log-out-symbolic\0037info\0037logout\n"
+	echo "Suspend\0icon\0037system-suspend-symbolic\0037info\0037suspend\n"
+	echo "Hibernate\0icon\0037system-hibernate-symbolic\0037info\0037hibernate\n"
+	echo "Reboot\0icon\0037system-reboot-symbolic\0037info\0037reboot\n"
+	echo "Shutdown\0icon\0037system-shutdown-symbolic\0037info\0037shutdown\n"
 elif [ "$ROFI_RETV" = "1" ]; then
 	case $ROFI_INFO in
 	*lock)
