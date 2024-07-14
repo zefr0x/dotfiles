63fa7544ee52e3ad3bb57f102a5620266ad3b1e1
zefr0x <zer0-x.7ty50@aleeas.com>
2024-07-14 18:22:41 +0300

    scripts(auto_display_settings): improvements to allow manual change via waybar

--- scripts/auto_display_settings.py
+++ scripts/auto_display_settings.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 """Change display settings depending on dawn/dusk time to save my eyes."""
 
+import json
 import subprocess
 from datetime import datetime
 from os import environ
@@ -16,49 +17,92 @@ DUSK_TIME = 64800.0  # 6pm (Shoulbe be > WINDOW + DAWN_TIME)
 # Time during which the temperature changes gradually until it reaches the desired value
 WINDOW = 900  # 15m
 
-control_file = Path(environ["XDG_RUNTIME_DIR"]).joinpath(
-    "auto-display-settings-disabled"
-)
-temp = None
 
-# NOTE: This is done only one time when the script starts, which reduces accuracy.
-now = datetime.now().astimezone()
-# Initiate with seconds since the beginning of day
-current_time = now.hour * 3600 + now.minute * 60 + now.second
+def set_temperature(temp: int) -> None:
+    """Send a requst to rs.wl-gammarelay to update the display temperature."""
+    subprocess.run(  # noqa: S603
+        [
+            "/usr/bin/busctl",
+            "--user",
+            "set-property",
+            "rs.wl-gammarelay",
+            "/",
+            "rs.wl.gammarelay",
+            "Temperature",
+            "q",
+            str(temp),
+        ]
+    )
 
-while True:
-    if DAWN_TIME - WINDOW < current_time < DAWN_TIME:
-        temp = (DAWN_TIME - current_time) * (TEMP_NIGHT - TEMP_DAY) / WINDOW + TEMP_DAY
-    elif DUSK_TIME - WINDOW < current_time < DUSK_TIME:
-        temp = (DUSK_TIME - current_time) * (
-            TEMP_DAY - TEMP_NIGHT
-        ) / WINDOW + TEMP_NIGHT
-    elif current_time >= DUSK_TIME or current_time < DAWN_TIME:
-        temp = TEMP_NIGHT
-    elif current_time >= DAWN_TIME or current_time < DUSK_TIME:
-        temp = TEMP_DAY
 
-    if temp:
-        subprocess.run(
-            [  # noqa: S603
-                "/usr/bin/busctl",
-                "--user",
-                "set-property",
-                "rs.wl-gammarelay",
-                "/",
-                "rs.wl.gammarelay",
-                "Temperature",
-                "q",
-                str(int(temp)),
-            ]
-        )
+def get_temperature() -> int:
+    """Send a requst for current temperature value from rs.wl-gammarelay."""
+    process = subprocess.Popen(  # noqa: S603
+        [
+            "/usr/bin/busctl",
+            "--user",
+            "get-property",
+            "rs.wl-gammarelay",
+            "/",
+            "rs.wl.gammarelay",
+            "Temperature",
+            "-j",
+        ],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.DEVNULL,
+    )
+    stdout, _ = process.communicate()
 
-    # NOTE: Calculatig current_time manually improves performance, but reduces accuracy.
-    current_time += 2
+    return json.loads(stdout.decode("utf-8")).get("data")
+
+
+def main() -> int:
+    """Entry point of the script."""
+    control_file = Path(environ["XDG_RUNTIME_DIR"]).joinpath(
+        "auto-display-settings-disabled"
+    )
+    settings_reverted_to_original = False
+    original_temp = get_temperature()
+    temp = None
+
+    # NOTE: This is done only one time when the script starts, which reduces accuracy.
+    now = datetime.now().astimezone()
+    # Initiate with seconds since the beginning of day
+    current_time = now.hour * 3600 + now.minute * 60 + now.second
 
     while True:
-        sleep(2)
+        if DAWN_TIME - WINDOW < current_time < DAWN_TIME:
+            temp = (DAWN_TIME - current_time) * (
+                TEMP_NIGHT - TEMP_DAY
+            ) / WINDOW + TEMP_DAY
+        elif DUSK_TIME - WINDOW < current_time < DUSK_TIME:
+            temp = (DUSK_TIME - current_time) * (
+                TEMP_DAY - TEMP_NIGHT
+            ) / WINDOW + TEMP_NIGHT
+        elif current_time >= DUSK_TIME or current_time < DAWN_TIME:
+            temp = TEMP_NIGHT
+        elif current_time >= DAWN_TIME or current_time < DUSK_TIME:
+            temp = TEMP_DAY
 
-        # Method to stop auto change of settings (for manual change).
-        if not control_file.is_file():
-            break
+        if temp:
+            set_temperature(int(temp))
+
+        # NOTE: Calculatig time manually improves performance, but reduces accuracy.
+        current_time += 2
+
+        while True:
+            sleep(2)
+
+            # Method to stop auto change of settings (for manual change).
+            if control_file.is_file():
+                if not settings_reverted_to_original:
+                    # Revert to original temperature
+                    set_temperature(original_temp)
+                    settings_reverted_to_original = True
+            else:
+                settings_reverted_to_original = False
+                break
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
